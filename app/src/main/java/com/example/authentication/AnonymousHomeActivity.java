b0b5f665d741ae2b32652498ad66b9f5baec2ebc
dorazouri <dorazouri@gmail.com>
2018-04-21 23:57:56 +0300

    View changes

--- app/src/main/java/com/example/authentication/AnonymousHomeActivity.java
+++ app/src/main/java/com/example/authentication/AnonymousHomeActivity.java
@@ -10,6 +10,8 @@ public class AnonymousHomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anonymous_home);
-        ((TextView) findViewById(R.id.AnonymousHomeEditText)).setText(R.string.anonymous_user_home_text);
+        TextView anonymousHomeTextView = (TextView) findViewById(R.id.AnonymousHomeEditText);
+        anonymousHomeTextView.setText(R.string.anonymous_user_home_text);
+        anonymousHomeTextView.setKeyListener(null);
     }
 }
